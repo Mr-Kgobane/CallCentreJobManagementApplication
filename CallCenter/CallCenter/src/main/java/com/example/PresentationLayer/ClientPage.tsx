511d2b8946671080169f50b8dc91032c82712744
Estavao <111559948+Estavao@users.noreply.github.com>
2023-10-29 15:45:12 +0200

    Create ClientPage.tsx

---
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/ClientPage.tsx
@@ -0,0 +1,78 @@
+import { FunctionComponent } from "react";
+import "./CLientPage.css";
+
+const CLientPage: FunctionComponent = () => {
+  return (
+    <div className="client-page">
+      <div className="client-page1">
+        <div className="client-page-child" />
+        <div className="client-page-item" />
+        <div className="place-call">PLACE CALL</div>
+        <div className="client-page-inner" />
+        <div className="john-doe-">John Doe -</div>
+        <div className="ai">AI</div>
+        <img className="rectangle-icon" alt="" src="/rectangle-4175.svg" />
+        <img className="client-page-child1" alt="" src="/rectangle-4217.svg" />
+        <div className="line-div" />
+        <div className="client-page-child2" />
+        <div className="done">Done</div>
+        <div className="pending">Pending</div>
+        <div className="initiated">Initiated</div>
+        <div className="call-history">Call History</div>
+        <div className="this-week">This Week</div>
+        <div className="last-week">Last Week</div>
+        <div className="last-month">Last Month</div>
+        <div className="rectangle-div" />
+        <div className="client-page-child3" />
+        <div className="client-page-child4" />
+        <div className="client-page-child5" />
+        <div className="client-page-child6" />
+        <div className="client-page-child7" />
+        <div className="client-page-child8" />
+        <div className="client-page-child9" />
+        <div className="client-page-child10" />
+        <div className="client-page-child11" />
+        <div className="client-page-child12" />
+        <div className="client-page-child13" />
+        <div className="client-page-child14" />
+        <div className="client-page-child15" />
+        <div className="client-page-child16" />
+        <div className="component-1">
+          <div className="component-1-child" />
+          <div className="login1">LOGOUT</div>
+        </div>
+        <img
+          className="icon-call-received-1-1"
+          alt=""
+          src="/-icon--call-received--1-1@2x.png"
+        />
+        <img
+          className="icon-call-received-5"
+          alt=""
+          src="/-icon--call-received--5@2x.png"
+        />
+        <img className="icon-person-1" alt="" src="/-icon--person--1@2x.png" />
+      </div>
+      <div className="outgoing-call">Outgoing Call</div>
+      <div className="plumbing-req">Plumbing req</div>
+      <div className="computer-req">Computer req</div>
+      <div className="analyst-req">Analyst req</div>
+      <div className="mechanic-req">Mechanic req</div>
+      <div className="it-technician-req">IT technician req</div>
+      <div className="electrician-req">Electrician req</div>
+      <div className="outgoing-call1">Outgoing Call</div>
+      <div className="outgoing-call2">Outgoing Call</div>
+      <div className="outgoing-call3">Outgoing Call</div>
+      <div className="outgoing-call4">Outgoing Call</div>
+      <div className="outgoing-call5">Outgoing Call</div>
+      <div className="div">05:05</div>
+      <div className="div1">01:20</div>
+      <div className="div2">02:23</div>
+      <div className="div3">04:15</div>
+      <div className="div4">00:59</div>
+      <div className="div5">03:59</div>
+    </div>
+  );
+};
+
+export default CLientPage;
