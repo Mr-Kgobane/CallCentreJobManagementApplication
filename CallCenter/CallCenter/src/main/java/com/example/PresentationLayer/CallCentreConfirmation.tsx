804b8231e17dba45cd826a3da78c186a7e50e452
Estavao <111559948+Estavao@users.noreply.github.com>
2023-10-29 16:52:38 +0200

    Create CallCentreConfirmation.tsx

---
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/CallCentreConfirmation.tsx
@@ -0,0 +1,97 @@
+import { FunctionComponent } from "react";
+import "./CallCentreConfirmation.css";
+
+const CallCentreConfirmation: FunctionComponent = () => {
+  return (
+    <div className="call-centre-confirmation">
+      <div className="dashboard">
+        <div className="dashboard-child" />
+        <img className="dashboard-item" alt="" src="/ellipse-1@2x.png" />
+        <div className="dashboard-inner" />
+        <div className="call-history">Call history</div>
+        <div className="ticket-details">Ticket Details</div>
+        <div className="rectangle-div" />
+        <div className="call-center">Call center</div>
+        <div className="dashboard-child1" />
+        <div className="dashboard-child2" />
+        <div className="contract">Contract</div>
+        <div className="dashboard-child3" />
+        <div className="dashboard-child4" />
+        <div className="dashboard-child5" />
+        <div className="dashboard-child6" />
+        <div className="dashboard-child7" />
+        <div className="dashboard-child8" />
+        <div className="dashboard-child9" />
+        <div className="ai">AI</div>
+        <div className="ticket-confimation-page">Ticket Confimation Page</div>
+        <img className="dashboard-child10" alt="" src="/rectangle-41721.svg" />
+        <div className="service-type">SERVICE TYPE:</div>
+        <img className="ellipse-icon" alt="" src="/ellipse-3@2x.png" />
+        <img className="ellipse-icon" alt="" src="/ellipse-3@2x.png" />
+        <div className="line-div" />
+        <div className="call-agent">Call Agent</div>
+        <div className="decline-wrapper">
+          <div className="decline1">End call</div>
+        </div>
+        <div className="summary">Summary:</div>
+        <div className="plumbing-wrapper">
+          <div className="plumbing1">PLUMBING</div>
+        </div>
+        <div className="analyst-wrapper">
+          <div className="plumbing1">ANALYST</div>
+        </div>
+        <div className="electrician-wrapper">
+          <div className="plumbing1">ELECTRICIAN</div>
+        </div>
+        <div className="mechanic-wrapper">
+          <div className="plumbing1">MECHANIC</div>
+        </div>
+        <div className="it-technitian-wrapper">
+          <div className="plumbing1">IT TECHNITIAN</div>
+        </div>
+        <div className="home-address-wrapper">
+          <div className="plumbing1">HOME ADDRESS</div>
+        </div>
+        <div className="other-address-wrapper">
+          <div className="plumbing1">OTHER ADDRESS</div>
+        </div>
+        <div className="jane-wrapper">
+          <div className="jane1">JANE</div>
+        </div>
+        <div className="sam-wrapper">
+          <div className="plumbing1">SAM</div>
+        </div>
+        <div className="john-wrapper">
+          <div className="jane1">JOHN</div>
+        </div>
+        <div className="elice-wrapper">
+          <div className="jane1">ELICE</div>
+        </div>
+        <div className="job-1-wrapper">
+          <div className="job-11">JOB 1</div>
+        </div>
+        <div className="job-2-wrapper">
+          <div className="jane1">JOB 2</div>
+        </div>
+        <div className="component-1">
+          <div className="component-1-child" />
+          <div className="login1">LOGOUT</div>
+        </div>
+      </div>
+      <div className="incoming-call">Incoming Call</div>
+      <div className="john-doe">John Doe</div>
+      <div className="accept-wrapper">
+        <div className="decline1">answer call</div>
+      </div>
+      <div className="location">LOCATION</div>
+      <div className="technician">TECHNICIAN</div>
+      <div className="job-type">JOB TYPE</div>
+      <div className="create-ticket-wrapper">
+        <div className="decline1">CONFIRM TICKET</div>
+      </div>
+      <div className="contract-details">Contract Details</div>
+    </div>
+  );
+};
+
+export default CallCentreConfirmation;
