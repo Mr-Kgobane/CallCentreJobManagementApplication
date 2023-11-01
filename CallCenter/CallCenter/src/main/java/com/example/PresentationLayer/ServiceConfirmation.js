30b18e296331d965a9848dbd544675ab6219637f
Estavao <111559948+Estavao@users.noreply.github.com>
2023-11-01 09:03:58 +0200

    Update and rename ServiceConfirmation.tsx to ServiceConfirmation.js

---
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/ServiceConfirmation.js
@@ -0,0 +1,119 @@
+import "./ServiceConfirmation.css";
+
+const ServiceConfirmation = () => {
+  return (
+    <div className="service-confirmation">
+      <div className="dashboard4">
+        <div className="dashboard-child46" />
+        <div className="dashboard-child47" />
+        <img className="dashboard-child48" alt="" src="/ellipse-1@2x.png" />
+        <div className="dashboard-child49" />
+        <div className="call-history5">Call history</div>
+        <div className="ticket-details4">Ticket Details</div>
+        <div className="dashboard-child50" />
+        <div className="call-center3">Call center</div>
+        <div className="dashboard-child51" />
+        <div className="dashboard-child52" />
+        <div className="contract3">Contract</div>
+        <div className="dashboard-child53" />
+        <div className="dashboard-child54" />
+        <div className="dashboard-child55" />
+        <div className="dashboard-child56" />
+        <div className="dashboard-child57" />
+        <div className="dashboard-child58" />
+        <div className="ai4">AI</div>
+        <img className="dashboard-child59" alt="" src="/rectangle-4172.svg" />
+        <div className="r-25000-1">R 25000 - upfront</div>
+        <div className="a79dfd191">a79dfd19</div>
+        <img className="dashboard-child60" alt="" src="/ellipse-3@2x.png" />
+        <img className="dashboard-child60" alt="" src="/ellipse-3@2x.png" />
+        <img className="dashboard-child60" alt="" src="/ellipse-3@2x.png" />
+        <div className="dashboard-child63" />
+        <div className="service-dept1">Service Dept.</div>
+        <div className="contracts1">Contracts:</div>
+        <div className="duration2">Duration:</div>
+        <div className="plumbing-wrapper1">
+          <div className="plumbing4">Type D</div>
+        </div>
+        <div className="analyst-wrapper1">
+          <div className="plumbing4">Type A</div>
+        </div>
+        <div className="analyst-wrapper2">
+          <div className="plumbing4">1 day</div>
+        </div>
+        <div className="electrician-wrapper10">
+          <div className="plumbing4">Type B</div>
+        </div>
+        <div className="electrician-wrapper11">
+          <div className="plumbing4">2 Days</div>
+        </div>
+        <div className="electrician-wrapper12">
+          <div className="plumbing4">5 days</div>
+        </div>
+        <div className="electrician-wrapper13">
+          <div className="plumbing4">1 week</div>
+        </div>
+        <div className="electrician-wrapper14">
+          <div className="plumbing4">2 weeks</div>
+        </div>
+        <div className="electrician-wrapper15">
+          <div className="plumbing4">1 month</div>
+        </div>
+        <div className="electrician-wrapper16">
+          <div className="plumbing4">2 months</div>
+        </div>
+        <div className="electrician-wrapper17">
+          <div className="plumbing4">6 months</div>
+        </div>
+        <div className="electrician-wrapper18">
+          <div className="plumbing4">1 year</div>
+        </div>
+        <div className="electrician-wrapper19">
+          <div className="plumbing4">2 years</div>
+        </div>
+        <div className="mechanic-wrapper1">
+          <div className="plumbing4">Type C</div>
+        </div>
+        <div className="it-technitian-wrapper1">
+          <div className="plumbing4">Type E</div>
+        </div>
+        <div className="home-address-wrapper4">
+          <div className="plumbing4">Business</div>
+        </div>
+        <div className="home-address-wrapper5">
+          <div className="plumbing4">50% Deposit</div>
+        </div>
+        <div className="home-address-wrapper6">
+          <div className="plumbing4">FULL</div>
+        </div>
+        <div className="home-address-wrapper7">
+          <div className="plumbing4">Installment</div>
+        </div>
+        <div className="other-address-wrapper2">
+          <div className="plumbing4">Basic</div>
+        </div>
+        <div className="other-address-wrapper3">
+          <div className="plumbing4">Premium</div>
+        </div>
+        <div className="component-14">
+          <div className="component-1-child2" />
+          <div className="login5">LOGOUT</div>
+        </div>
+        <div className="contract-details4">Contract Details</div>
+      </div>
+      <div className="client2">CLIENT:</div>
+      <div className="ticket1">TICKET:</div>
+      <div className="john-doe4">John Doe</div>
+      <div className="package1">Package:</div>
+      <div className="payment-option1">Payment option:</div>
+      <div className="cost1">Cost:</div>
+      <div className="create-ticket-wrapper1">
+        <div className="create-ticket4">CONFIRM CONTRACT</div>
+      </div>
+      <div className="service-confirmation-page">Service Confirmation Page</div>
+    </div>
+  );
+};
+
+export default ServiceConfirmation;
+
