19206e205dbdfd2730e018d77e8d159f030736e3
Estavao <111559948+Estavao@users.noreply.github.com>
2023-11-01 09:06:22 +0200

    Update and rename ContactDetails.tsx to ContactDetails.js

--- CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/ContactDetails.js
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/ContactDetails.js
@@ -1,15 +1,15 @@
-import { FunctionComponent, useCallback } from "react";
+import { useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "./ContactDetails.css";
 
-const ContactDetails: FunctionComponent = () => {
+const ContactDetails = () => {
   const navigate = useNavigate();
 
   const onCallHistoryTextClick = useCallback(() => {
-    // Please sync "Call History" to the project
-  }, []);
+    navigate("/call-history");
+  }, [navigate]);
 
   const onTicketDetailsTextClick = useCallback(() => {
     navigate("/ticket");
@@ -20,50 +20,54 @@ const ContactDetails: FunctionComponent = () => {
       <div className="contact-details-child" />
       <div className="contact-details-item" />
       <div className="contact-details-inner" />
-      <img className="rectangle-icon" alt="" src="/rectangle-4175.svg" />
-      <img className="ellipse-icon" alt="" src="/ellipse-1@2x.png" />
-      <div className="rectangle-div" />
-      <div className="call-history" onClick={onCallHistoryTextClick}>
+      <img
+        className="contact-details-child1"
+        alt=""
+        src="/rectangle-4172.svg"
+      />
+      <img className="contact-details-child2" alt="" src="/ellipse-1@2x.png" />
+      <div className="contact-details-child3" />
+      <div className="call-history6" onClick={onCallHistoryTextClick}>
         Call history
       </div>
-      <div className="ticket-details" onClick={onTicketDetailsTextClick}>
+      <div className="ticket-details5" onClick={onTicketDetailsTextClick}>
         Ticket Details
       </div>
-      <div className="contact-details-child1" />
-      <div className="contact-details-child2" />
-      <div className="contact-details-child3" />
       <div className="contact-details-child4" />
       <div className="contact-details-child5" />
       <div className="contact-details-child6" />
       <div className="contact-details-child7" />
       <div className="contact-details-child8" />
-      <div className="ai">AI</div>
+      <div className="contact-details-child9" />
+      <div className="contact-details-child10" />
+      <div className="contact-details-child11" />
+      <div className="ai5">AI</div>
       <div className="ytsd">5554518ytsd</div>
-      <div className="r-25000-">R 25000 - upfront</div>
+      <div className="r-25000-2">R 25000 - upfront</div>
       <div className="deposit">50% Deposit</div>
       <div className="type-a">Type A</div>
       <div className="account">Account:</div>
-      <div className="duration">DUration</div>
+      <div className="duration3">DUration</div>
       <div className="contract-type">Contract Type</div>
-      <div className="line-div" />
-      <div className="admin">ADMIN</div>
-      <div className="component-1">
+      <div className="contact-details-child12" />
+      <div className="admin1">ADMIN</div>
+      <div className="component-15">
         <Form className="wrapper">
           <Form.Control type="text" />
         </Form>
-        <div className="login1">LOGOUT</div>
+        <div className="login6">LOGOUT</div>
       </div>
       <div className="paymnent-option">Paymnent Option:</div>
-      <div className="cost">Cost:</div>
-      <div className="package">Package</div>
+      <div className="cost2">Cost:</div>
+      <div className="package2">Package</div>
       <div className="weeks">{`2 weeks `}</div>
       <div className="premium">Premium</div>
-      <div className="contract-details">Contract Details</div>
-      <img className="contact-details-child9" alt="" src="/ellipse-6@2x.png" />
-      <div className="client">CLIENT:</div>
-      <div className="john-doe">John Doe</div>
-      <div className="ticket">TICKET:</div>
-      <div className="a79dfd19">a79dfd19</div>
+      <div className="contract-details5">Contract Details</div>
+      <img className="contact-details-child13" alt="" src="/ellipse-6@2x.png" />
+      <div className="client3">CLIENT:</div>
+      <div className="john-doe5">John Doe</div>
+      <div className="ticket2">TICKET:</div>
+      <div className="a79dfd192">a79dfd19</div>
       <div className="contract-details-page">Contract Details Page</div>
     </div>
   );
