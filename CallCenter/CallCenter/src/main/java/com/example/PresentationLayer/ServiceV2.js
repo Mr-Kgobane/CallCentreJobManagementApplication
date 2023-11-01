f6421f39703514ceb69eef27965fafe9b599d41b
Estavao <111559948+Estavao@users.noreply.github.com>
2023-11-01 09:01:45 +0200

    Update and rename ServiceV2.tsx to ServiceV2.js

---
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/ServiceV2.js
@@ -0,0 +1,119 @@
+import "./ServiceV2.css";
+
+const ServiceV2 = () => {
+  return (
+    <div className="service-v2">
+      <div className="dashboard3">
+        <div className="dashboard-child28" />
+        <div className="dashboard-child29" />
+        <img className="dashboard-child30" alt="" src="/ellipse-1@2x.png" />
+        <div className="dashboard-child31" />
+        <div className="call-history4">Call history</div>
+        <div className="ticket-details3">Ticket Details</div>
+        <div className="dashboard-child32" />
+        <div className="call-center2">Call center</div>
+        <div className="dashboard-child33" />
+        <div className="dashboard-child34" />
+        <div className="contract2">Contract</div>
+        <div className="dashboard-child35" />
+        <div className="dashboard-child36" />
+        <div className="dashboard-child37" />
+        <div className="dashboard-child38" />
+        <div className="dashboard-child39" />
+        <div className="dashboard-child40" />
+        <div className="ai3">AI</div>
+        <img className="dashboard-child41" alt="" src="/rectangle-4172.svg" />
+        <div className="r-25000-">R 25000 - upfront</div>
+        <div className="a79dfd19">a79dfd19</div>
+        <img className="dashboard-child42" alt="" src="/ellipse-3@2x.png" />
+        <img className="dashboard-child42" alt="" src="/ellipse-3@2x.png" />
+        <img className="dashboard-child42" alt="" src="/ellipse-3@2x.png" />
+        <div className="dashboard-child45" />
+        <div className="service-dept">Service Dept.</div>
+        <div className="contracts">Contracts:</div>
+        <div className="duration1">Duration:</div>
+        <div className="plumbing-frame">
+          <div className="plumbing3">Type D</div>
+        </div>
+        <div className="analyst-frame">
+          <div className="plumbing3">Type A</div>
+        </div>
+        <div className="frame-div">
+          <div className="plumbing3">1 day</div>
+        </div>
+        <div className="electrician-frame">
+          <div className="plumbing3">Type B</div>
+        </div>
+        <div className="electrician-wrapper1">
+          <div className="plumbing3">2 Days</div>
+        </div>
+        <div className="electrician-wrapper2">
+          <div className="plumbing3">5 days</div>
+        </div>
+        <div className="electrician-wrapper3">
+          <div className="plumbing3">1 week</div>
+        </div>
+        <div className="electrician-wrapper4">
+          <div className="plumbing3">2 weeks</div>
+        </div>
+        <div className="electrician-wrapper5">
+          <div className="plumbing3">1 month</div>
+        </div>
+        <div className="electrician-wrapper6">
+          <div className="plumbing3">2 months</div>
+        </div>
+        <div className="electrician-wrapper7">
+          <div className="plumbing3">6 months</div>
+        </div>
+        <div className="electrician-wrapper8">
+          <div className="plumbing3">1 year</div>
+        </div>
+        <div className="electrician-wrapper9">
+          <div className="plumbing3">2 years</div>
+        </div>
+        <div className="mechanic-frame">
+          <div className="plumbing3">Type C</div>
+        </div>
+        <div className="it-technitian-frame">
+          <div className="plumbing3">Type E</div>
+        </div>
+        <div className="home-address-frame">
+          <div className="plumbing3">Business</div>
+        </div>
+        <div className="home-address-wrapper1">
+          <div className="plumbing3">50% Deposit</div>
+        </div>
+        <div className="home-address-wrapper2">
+          <div className="plumbing3">FULL</div>
+        </div>
+        <div className="home-address-wrapper3">
+          <div className="plumbing3">Installment</div>
+        </div>
+        <div className="other-address-frame">
+          <div className="plumbing3">Basic</div>
+        </div>
+        <div className="other-address-wrapper1">
+          <div className="plumbing3">Premium</div>
+        </div>
+        <div className="component-13">
+          <div className="component-1-child1" />
+          <div className="login4">LOGOUT</div>
+        </div>
+        <div className="contract-details3">Contract Details</div>
+      </div>
+      <div className="client1">CLIENT:</div>
+      <div className="ticket">TICKET:</div>
+      <div className="john-doe3">John Doe</div>
+      <div className="package">Package:</div>
+      <div className="payment-option">Payment option:</div>
+      <div className="cost">Cost:</div>
+      <div className="create-ticket-frame">
+        <div className="create-ticket3">CREATE CONTRACT</div>
+      </div>
+      <div className="service-assignment-page">Service assignment Page</div>
+    </div>
+  );
+};
+
+export default ServiceV2;
+
