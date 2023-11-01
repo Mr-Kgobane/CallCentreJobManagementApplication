2b1130678ee905cdfe5adbf31b821c54c306c0b5
Estavao <111559948+Estavao@users.noreply.github.com>
2023-11-01 08:57:59 +0200

    Update and rename CallCentreV2.tsx to CallCentreV2.js

---
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/CallCentreV2.js
@@ -0,0 +1,95 @@
+import "./CallCentreV2.css";
+
+const CallCentreV2 = () => {
+  return (
+    <div className="call-centre-v2">
+      <div className="dashboard2">
+        <img className="dashboard-child11" alt="" src="/rectangle-4172.svg" />
+        <div className="dashboard-child12" />
+        <img className="dashboard-child13" alt="" src="/ellipse-1@2x.png" />
+        <div className="dashboard-child14" />
+        <div className="call-history3">Call history</div>
+        <div className="ticket-details2">Ticket Details</div>
+        <div className="dashboard-child15" />
+        <div className="call-center1">Call center</div>
+        <div className="dashboard-child16" />
+        <div className="dashboard-child17" />
+        <div className="contract1">Contract</div>
+        <div className="dashboard-child18" />
+        <div className="dashboard-child19" />
+        <div className="dashboard-child20" />
+        <div className="dashboard-child21" />
+        <div className="dashboard-child22" />
+        <div className="dashboard-child23" />
+        <div className="dashboard-child24" />
+        <div className="ai2">AI</div>
+        <div className="ticket-assignment-page">Ticket assignment Page</div>
+        <img className="dashboard-child25" alt="" src="/ellipse-3@2x.png" />
+        <img className="dashboard-child25" alt="" src="/ellipse-3@2x.png" />
+        <div className="dashboard-child27" />
+        <div className="call-agent1">Call Agent</div>
+        <div className="decline-container">
+          <div className="decline2">End call</div>
+        </div>
+        <div className="job-type1">JOB TYPE</div>
+        <div className="plumbing-container">
+          <div className="plumbing2">PLUMBING</div>
+        </div>
+        <div className="analyst-container">
+          <div className="plumbing2">ANALYST</div>
+        </div>
+        <div className="electrician-container">
+          <div className="plumbing2">ELECTRICIAN</div>
+        </div>
+        <div className="mechanic-container">
+          <div className="plumbing2">MECHANIC</div>
+        </div>
+        <div className="it-technitian-container">
+          <div className="plumbing2">IT TECHNITIAN</div>
+        </div>
+        <div className="home-address-container">
+          <div className="plumbing2">HOME ADDRESS</div>
+        </div>
+        <div className="other-address-container">
+          <div className="plumbing2">OTHER ADDRESS</div>
+        </div>
+        <div className="jane-container">
+          <div className="jane2">JANE</div>
+        </div>
+        <div className="sam-container">
+          <div className="jane2">SAM</div>
+        </div>
+        <div className="john-container">
+          <div className="jane2">JOHN</div>
+        </div>
+        <div className="elice-container">
+          <div className="jane2">ELICE</div>
+        </div>
+        <div className="job-1-container">
+          <div className="jane2">JOB 1</div>
+        </div>
+        <div className="job-2-container">
+          <div className="jane2">JOB 2</div>
+        </div>
+        <div className="component-12">
+          <div className="component-1-inner" />
+          <div className="login3">LOGOUT</div>
+        </div>
+      </div>
+      <div className="incoming-call1">Incoming Call</div>
+      <div className="john-doe2">John Doe</div>
+      <div className="accept-container">
+        <div className="decline2">answer call</div>
+      </div>
+      <div className="location1">LOCATION</div>
+      <div className="technician1">TECHNICIAN</div>
+      <div className="client-history">CLIENT HISTORY</div>
+      <div className="create-ticket-container">
+        <div className="decline2">CREATE TICKET</div>
+      </div>
+      <div className="contract-details2">Contract Details</div>
+    </div>
+  );
+};
+
+export default CallCentreV2;
