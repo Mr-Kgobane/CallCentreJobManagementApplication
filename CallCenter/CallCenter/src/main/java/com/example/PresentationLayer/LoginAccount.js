98f4accb2198b597bfe791170c04e4f305b4a84d
Estavao <111559948+Estavao@users.noreply.github.com>
2023-11-01 09:11:32 +0200

    Update and rename LoginAccount.tsx to LoginAccount.js

--- CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/LoginAccount.js
+++ CallCenter/CallCenter/src/main/java/com/example/PresentationLayer/LoginAccount.js
@@ -1,21 +1,20 @@
-import { FunctionComponent } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import "./LoginAccount.css";
 
-const LoginAccount: FunctionComponent = () => {
+const LoginAccount = () => {
   return (
     <div className="login-account">
       <div className="login-account-child" />
-      <div className="ai">AI</div>
+      <div className="ai7">AI</div>
       <div className="login-account-item" />
-      <div className="login1">LOGIN</div>
+      <div className="login8">LOGIN</div>
       <div className="login-account-inner" />
       <div className="username">Username</div>
       <div className="password">Password</div>
-      <img className="rectangle-icon" alt="" src="/rectangle-4151.svg" />
+      <img className="login-account-child1" alt="" src="/rectangle-4151.svg" />
       <div className="forgot-usernamepassword">forgot username/password</div>
-      <Button className="component-1" variant="outline-primary">
+      <Button className="component-17" variant="outline-primary">
         LOGIN
       </Button>
     </div>
